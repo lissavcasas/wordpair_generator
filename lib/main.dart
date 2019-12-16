67cb6215e481c2a8a32db83b38301dec2f219f96
marimeli <marimel.casas@gmail.com>
2019-12-16 11:28:37 -0500

    add appbar and body text example

---
+++ lib/main.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+/* tatu */
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: ThemeData(primaryColor: Colors.red),
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('WordPair Generator'),
+        ),
+        body: Center(
+          child: Text('Holo')
+        )
+      ),
+    );
+  }
+}
