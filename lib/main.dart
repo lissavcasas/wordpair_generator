2d5ca3ad0108820522c9624e0200e1e80e73e6f6
marimeli <marimel.casas@gmail.com>
2019-12-16 12:35:23 -0500

    add static view list

--- lib/main.dart
+++ lib/main.dart
@@ -9,7 +9,8 @@ class MyApp extends StatelessWidget {
     final wordPair = WordPair.random();
     return MaterialApp(
         theme: ThemeData(primaryColor: Colors.red), 
-        home: RandomWords());
+        home: RandomWords()
+    );
   }
 }
 
@@ -19,9 +20,36 @@ class RandomWords extends StatefulWidget {
 }
 
 class RandomWordsState extends State<RandomWords> {
+
+  Widget _buildList(){
+    return ListView(
+      padding: const EdgeInsets.all(8),
+      children: <Widget>[
+        Container(
+          height: 50,
+          color: Colors.amber[600],
+          child: const Center(child: Text('Entry A')),
+        ),
+        Container(
+          height: 50,
+          color: Colors.amber[500],
+          child: const Center(child: Text('Entry B')),
+        ),
+        Container(
+          height: 50,
+          color: Colors.amber[100],
+          child: const Center(child: Text('Entry C')),
+        ),
+      ],
+    );
+  }
+
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text('WordPair Generator')),
+      appBar: AppBar(
+        title: Text('WordPair Generator')
+      ),
+      body: _buildList() 
     );
   }
 }
